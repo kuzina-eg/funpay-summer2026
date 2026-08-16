723fac9ebcbb64c12f269683a5ff1925ae5b6eef
Ekaterina Kuzina <kuzina_eg@mail.ru>
2026-08-16 22:20:57 +0600

    fix my tickets image

--- src/scripts/components/countDown.js
+++ src/scripts/components/countDown.js
@@ -3,7 +3,7 @@ export default function countDown() {
 
     if (countdown) {
         // Set the date we're counting down to
-        const countDownDate = new Date("Sep 15, 2026 00:00:00").getTime();
+        const countDownDate = new Date("Sep 16, 2026 00:00:00").getTime();
 
         // Update the count down every 1 second
         const x = setInterval(function () {
